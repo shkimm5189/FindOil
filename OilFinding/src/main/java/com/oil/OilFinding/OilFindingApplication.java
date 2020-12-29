6e9c3f81e59816b14b4069df4dbbd147f7cebde8
shkimm5189 <shkimm5189@gmail.com>
2020-12-29 16:56:15 +0900

    first commit

---
+++ OilFinding/src/main/java/com/oil/OilFinding/OilFindingApplication.java
@@ -0,0 +1,13 @@
+package com.oil.OilFinding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OilFindingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OilFindingApplication.class, args);
+	}
+
+}
---
+++ OilFinding/src/test/java/com/oil/OilFinding/OilFindingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oil.OilFinding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OilFindingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
