1014446775cf91505dec7f7a7186f6be3c90f92d
shkimm5189 <shkimm5189@gmail.com>
2021-01-03 11:31:41 +0900

    BaseTimeEntity에서 Entity변경시 createDate가 null로 변경되는 현상 해결

--- OilFinding/src/test/java/com/oil/OilFinding/testing/UserTest.java
+++ OilFinding/src/test/java/com/oil/OilFinding/testing/UserTest.java
@@ -26,4 +26,32 @@ public class UserTest {
         userRepository.save(user);
 
     }
+    @Test
+    public void read(){
+        User user = userRepository.findById(2L).orElse(null);
+        System.out.println(user.getEmail());
+        System.out.println(user.getPassword());
+    }
+    @Test
+    public void delete(){
+        User user = userRepository.findById(2L).orElse(null);
+        boolean chk = user.getPassword().equals("password");
+        if(chk){
+            userRepository.delete(user);
+        }
+    }
+    @Test
+    public void updatePasswd(){
+        User user = userRepository.findById(9L).orElse(null);
+        boolean chk = user.getEmail().equals("test@naver.com");
+        if(chk){
+            userRepository.save(User.builder()
+                    .userCode(user.getUserCode())
+                    .email(user.getEmail())
+                    .phoneNumber(user.getPhoneNumber())
+                    .password("changedpass")
+                    .build());
+        }
+    }
+
 }
