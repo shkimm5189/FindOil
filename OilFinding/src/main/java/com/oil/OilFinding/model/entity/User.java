57944a912d13345721388076a90ef9d47774b55d
shkimm5189 <shkimm5189@gmail.com>
2021-01-10 16:46:27 +0900

    feat: JwtConfig,Security 설정 완료
    Jwt 설정과 Security기본 설정 완료

--- OilFinding/src/main/java/com/oil/OilFinding/model/entity/User.java
+++ OilFinding/src/main/java/com/oil/OilFinding/model/entity/User.java
@@ -5,9 +5,17 @@ import com.oil.OilFinding.enumClass.Role;
 import com.oil.OilFinding.model.BaseTimeEntity;
 import com.sun.istack.Nullable;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Getter
@@ -16,7 +24,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Table(name = "user")
 @Entity
-public class User extends BaseTimeEntity{
+public class User extends BaseTimeEntity implements UserDetails {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(updatable = false,nullable = false,columnDefinition = "BIGINT")
     private Long userCode;
@@ -32,5 +40,41 @@ public class User extends BaseTimeEntity{
     private String phoneNumber;
     public String getRoleKey(){
         return this.role.getKey();
+    }
+
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
     }
 }
--- OilFinding/src/main/java/com/oil/OilFinding/service/UserService.java
+++ OilFinding/src/main/java/com/oil/OilFinding/service/UserService.java
@@ -1,6 +1,19 @@
 package com.oil.OilFinding.service;
 
-public class UserService {
+import com.oil.OilFinding.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+@RequiredArgsConstructor
+@Service
+public class UserService implements UserDetailsService {
+    private UserRepository userRepository;
 
-
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+    }
 }
